b71681197984b8a234e5a6acb5bc1772d04d6e29
Jonathan Wise <jonathan.wise@cesmii.org>
2020-04-13 19:39:53 -0400

    updates

--- windows/maphome.bat
+++ windows/maphome.bat
@@ -20,7 +20,7 @@ if !errorlevel! neq 0 (
 	powershell sleep -s 10
 )
 
-echo Mapping Drive...
+echo Mapping Drive %shareserver%\%sharepath%...
 net use h: /delete > nul 2>&1
 if "%1" neq "" (
 	net use h: \\%shareserver%\%sharepath% /user:%1 /persistent:no
