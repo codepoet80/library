295a8ced844419e75eabfd1a350933222bfab0d0
codepoet <jon.wise@jonandnic.com>
2024-09-23 12:52:52 -0400

    Add files via upload

---
+++ windows/synctime.bat
@@ -0,0 +1,16 @@
+@echo off
+echo Syncing Time...
+
+:: Check privileges 
+net file 1>NUL 2>NUL
+if not '%errorlevel%' == '0' (
+    powershell Start-Process -FilePath "%0" -ArgumentList "%cd%" -verb runas >NUL 2>&1
+    exit /b
+)
+
+:: Change directory with passed argument. Processes started with
+:: "runas" start with forced C:\Windows\System32 workdir
+cd /d %1
+
+:: Actual work
+C:\Windows\System32\w32tm.exe /resync /force
